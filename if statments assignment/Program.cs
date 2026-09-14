5452ba155d1e1306930693c86e490fbdf7629228
339821175 <339821175@2260I-L29-020>
2026-09-14 13:19:04 -0400

    Add project files.

---
+++ if statments assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_statments_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int degree;
+            Console.WriteLine("Enter from 0-360");
+            degree = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
