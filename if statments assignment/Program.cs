418a92b71c64bd8ff92d74a2a0db060310f99136
339821175 <339821175@2260I-L29-020>
2026-09-17 13:06:45 -0400

    done

--- if statments assignment/Program.cs
+++ if statments assignment/Program.cs
@@ -9,12 +9,13 @@ namespace if_statments_assignment
 {
     internal class Program
     {
+        //CURTIS APFELBECK
         static void Main(string[] args)
         {
             Console.WriteLine("Which part of the code would you like to run (choose between 1-3)");
             Console.WriteLine("1 - Compass Bearings");
-            Console.WriteLine("2 - Dice Roller");
-            Console.WriteLine("3 - Random Decimal Numbers");
+            Console.WriteLine("2 - Parking Garage Cost");
+            Console.WriteLine("3 - Hurricane");
 
             int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -66,22 +67,92 @@ namespace if_statments_assignment
             Console.Write("How many minutes have you been parked at the garage?: ");
             minutes = Convert.ToInt32(Console.ReadLine());
 
-            if (minutes <= 60) 
-            { 
-                cost += 40;
+            if (minutes <= 60)
+            {
+                Console.WriteLine("The cost of parking is: $4 for 1 hour");
             }
-            if (minutes >= 61 && minutes <= 120) 
+            else if (minutes >= 120)
+            {
+                Console.WriteLine("The cost of parking is: $6 for 2 hours");
+            }
+            else if (minutes >= 180)
+            {
+                Console.WriteLine("The cost of parking is: $8 for 3 hours");
+            }
+            else if (minutes >= 240)
+            {
+                Console.WriteLine("The cost of parking is: $10 for 4 hours");
+            }
+            else if (minutes >= 300)
+            {
+                Console.WriteLine("The cost of parking is: $12 for 5 hours");
+            }
+            else if (minutes >= 360)
+            {
+                Console.WriteLine("The cost of parking is: $14 for 6 hours");
+            }
+            else if (minutes >= 420)
+            {
+                Console.WriteLine("The cost of parking is: $16 for 7 hours");
+            }
+            else if (minutes >= 480)
+            {
+                Console.WriteLine("The cost of parking is: $18 for 8 hours");
+            }
+            else 
             { 
-                cost += 20;
+                Console.WriteLine("The maximum cost of parking is: $20 for a day");
             }
 
-            hours = cost / 60; 
-            Console.WriteLine($"The cost of parking is: {hours}");
+            
+            
         }
 
         public static void Part3()
         {
+            Console.Clear();
 
+            int input;
+
+            Console.Write("Enter a between 1-5 for a hurricane category: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Category 1 Hurricane Wind Speeds:");
+                    Console.WriteLine("- 74-95 mph");
+                    Console.WriteLine("- 64-82 kt");
+                    Console.WriteLine("- 119-153 km/hr");
+                    break;
+                case 2:
+                    Console.WriteLine("Category 2 Hurricane Wind Speeds:");
+                    Console.WriteLine("- 96-110 mph");
+                    Console.WriteLine("- 83-95 kt");
+                    Console.WriteLine("- 154-177 km/hr");
+                    break;
+                case 3:
+                    Console.WriteLine("Category 3 Hurricane Wind Speeds:");
+                    Console.WriteLine("- 11-130 mph");
+                    Console.WriteLine("- 96-113 kt");
+                    Console.WriteLine("- 178-209 km/hr");
+                    break;
+                case 4:
+                    Console.WriteLine("Category 4 Hurricane Wind Speeds:");
+                    Console.WriteLine("- 131-155 mph");
+                    Console.WriteLine("- 114-135 kt");
+                    Console.WriteLine("- 210-249 km/hr");
+                    break;
+                case 5:
+                    Console.WriteLine("Category 5 Hurricane Wind Speeds:");
+                    Console.WriteLine("- Greater than 155 mph");
+                    Console.WriteLine("- Greater than 135 kt");
+                    Console.WriteLine("- Greater than 249 km/hr");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 5.");
+                    break;
+            }
         }
     }
 }
